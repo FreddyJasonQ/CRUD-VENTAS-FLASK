9dba21bae0acace3f4169258b52bea5e5414d869
FreddyJasonQ <Freddyuj0@gmail.com>
2025-06-10 15:34:07 -0400

    Fix: crear tablas automáticamente

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, redirect, url_for
 
-from controllers import usuario_controller,cliente_controller,producto_controller,venta_controller
-
+from controllers import usuario_controller, cliente_controller, producto_controller, venta_controller
 from database import db
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ventas.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
+# Crear tablas automáticamente al iniciar la app (importante para Render)
+with app.app_context():
+    db.create_all()
+
+# Registrar Blueprints
 app.register_blueprint(usuario_controller.usuario_bp)
 app.register_blueprint(cliente_controller.cliente_bp)
 app.register_blueprint(producto_controller.producto_bp)
@@ -20,15 +24,11 @@ app.register_blueprint(venta_controller.venta_bp)
 def inject_active_path():
     def is_active(path):
         return 'active' if request.path == path else ''
-    return(dict(is_active = is_active))
-    
+    return dict(is_active=is_active)
 
 @app.route("/")
 def home():
     return redirect(url_for('usuario.index'))
 
-
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
